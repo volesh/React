702bdb701d708b5737b356e9bb32f517acbad73c
volesh <volesh2@gmail.com>
2022-09-18 11:32:02 +0300

    HW 2

--- src/components/users/User.js
+++
@@ -1,9 +0,0 @@
-
-export default function User({user}){
-    return(<div>
-        <div className={'info'}>
-            {user.id} - {user.name}
-        </div>
-        <button className={'button'}>Watch info</button>
-    </div>)
-}
