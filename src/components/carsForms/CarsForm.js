022feaa5b9ca04df03da74684fb1738d230eb4f4
volesh <volesh2@gmail.com>
2022-09-22 12:12:37 +0300

    HW 3 fin

--- src/components/carsForms/CarsForm.js
+++ src/components/carsForms/CarsForm.js
@@ -1,11 +1,18 @@
 import React, {useEffect} from 'react';
+import {joiResolver} from "@hookform/resolvers/joi";
+
 import {useForm} from "react-hook-form";
 import {carsService} from "../../services";
+import {carValidator} from "../../validators";
+import css from './carsForm.module.css'
 
 
 const CarsForm = ({setCars, carr, togle, btn}) => {
     let {id, price, model, year} = carr
-    const {register, handleSubmit, reset, formState:{errors, isValid}, setValue} = useForm({mode:'all'})
+    const {register, handleSubmit, reset, formState:{errors,isValid}, setValue} = useForm({
+        resolver:joiResolver(carValidator),
+        mode:'all'
+    })
 
     const submit = async (car) => {
         if (id && btn){
@@ -35,10 +42,25 @@ const CarsForm = ({setCars, carr, togle, btn}) => {
 
     return (
         <form onSubmit={handleSubmit(submit)}>
-            <input type="text" placeholder={'model'} {...register('model')}/>
-            <input type="number" placeholder={'price'} {...register('price', {valueAsNumber: true})}/>
-            <input type="number " placeholder={'year'} {...register('year', {valueAsNumber: true})}/>
-            <button disabled={!isValid}>{btn?'Update':'Save'}</button>
+
+            <div>
+                <div className={css.input}>
+                    <input type="text" placeholder={'model'} {...register('model')}/><br/>
+                    <div className={css.spanDiv}>{errors.model&&<span className={css.span}>{errors.model.message}</span>}</div>
+                </div>
+
+                <div className={css.input}>
+                    <input type="number" placeholder={'price'} {...register('price', {valueAsNumber: true})}/><br/>
+                    <div className={css.spanDiv}>{errors.price&&<span className={css.span}>{errors.price.message}</span>}</div>
+                </div>
+
+                <div className={css.input}>
+                    <input type="number " placeholder={'year'} {...register('year', {valueAsNumber: true})}/><br/>
+                    <div className={css.spanDiv}>{errors.year&&<span className={css.span}>{errors.year.message}</span>}</div>
+                </div>
+            </div>
+
+            <button className={css.btn} disabled={!isValid}>{btn?'Update':'Save'}</button>
         </form>
     );
 };
---
+++ src/validators/car.validator.js
@@ -0,0 +1,15 @@
+import Joi, {date} from "joi";
+
+const carValidator = Joi.object({
+    model: Joi.string().regex(/^[a-zA-ZА-яїЇєЄйЙіІгГ]{1,20}$/).required().messages({
+        'string.pattern.base':'Тільки букви,(1-20) символів'
+    }),
+    price:Joi.number().max(1000000).min(0).required().messages({
+        'number.pattern.base':'Від 0 до 1000000'
+    }),
+    year:Joi.number().min(1990).max(new Date().getFullYear()).required().messages({
+        'date.pattern.base':'Від 1990р. до сьогодні'
+    })
+})
+
+export {carValidator}
