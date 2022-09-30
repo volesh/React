2fe4c7a839a7228de48bcde2be335f344d9459f7
volesh <volesh2@gmail.com>
2022-09-30 16:45:59 +0300

    HW reducer

--- src/components/dog/Dog.js
+++ src/components/dog/Dog.js
@@ -2,10 +2,11 @@ import React from "react";
 import {useForm} from "react-hook-form";
 
 const Dog = ({dispatch}) => {
-    const {handleSubmit, register, formState:{isValid}} = useForm()
+    const {handleSubmit, register, formState:{isValid},reset} = useForm()
 
     const submit = (val) =>{
         dispatch({...val, id:'dog'})
+        reset()
     }
 
     return (
