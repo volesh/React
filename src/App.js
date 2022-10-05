67353fb32c4870253ff11e2f2005c7b66c85d35d
volesh <volesh2@gmail.com>
2022-10-05 17:01:30 +0300

    Login finish

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path={'/'} element={<Header/>}/>
+      <Route path={'/'} element={<Login/>}/>
     </Routes>
   );
 }
